0c7769eea1950affb940d53acfc337b6163e5e1a
Anton V. Yanchenko <simplylizz@gmail.com>
2013-02-01 22:30:47 +0400

    Removes unnecessary stuff from manage script.

--- manage.py
+++ manage.py
@@ -17,12 +17,6 @@ project_root_path = os.path.join(os.path.dirname(app.root_path))
 
 
 @manager.command
-def run():
-    """Run local server."""
-    app.run()
-
-
-@manager.command
 def initdb():
     """Reset and init database."""
 
@@ -43,23 +37,8 @@ def initdb():
 
     user = models.User(**user_defaults)
 
-    # First post from my sandbox.
-    post = models.Post(
-        author=user,
-        title=u'5 минут, полёт нормальный',
-        slug='pystuff-go-go-go',
-        body=u"""<p>Привет, мир!</p>\n<p>Запустился очередной сайт ("портал" звучало бы слишком пафосно) на питоне о питоне - pystuff.</p>\n<p>На самом деле я хочу изучить Flask. За одно заставить себя читать больше инфы о python, т.к. я выбрал его как основной скилл. Ну ещё хочется совсем чуть-чуть экспериментов по (ой!) SEO и маркетингу.</p>\n<p>За сим прощаюсь, всем спасибо за внимание, до новых встреч.</p>\n<p>P.S. Поехали!</p>""",
-    )
-
     db.session.add(user)
-    db.session.add(post)
     db.session.commit()
-
-
-manager.add_option('-c', '--config',
-                   dest="config",
-                   required=False,
-                   help="config file")
 
 
 if __name__ == "__main__":
